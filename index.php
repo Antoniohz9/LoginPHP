c62dee056ae01917f071ef0c6b1b3b2f36c19f90
Antonio Francisco <antoniohz2203@gmail.com>
2020-12-07 00:44:30 -0600

    add function of errors and script of sweet alert

--- index.php
+++ index.php
@@ -6,9 +6,28 @@
   <title>Login with PHP</title>
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
+	<script type="text/javascript" src="src/services/mensajes.js"> </script>
+  
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
-<body>
+	<script>
+	function verificar(){
+		<?php
+		switch($_REQUEST['error']){
+      case '1';
+        echo 'error("Tu usuario o contraseña están incorrectos, favor de verificar");';
+				break;
+			case '2';
+				echo 'error("Tu sesión ha caducado, favor de verificar");';
+				break;
+			case '3';
+				echo 'error("Gracias por su visita!");';
+				break;
+		}
+		?>
+	}
+	</script>
+<body onLoad="verificar();">
   <div class="container" style="margin-top:20px">
     <div class="card">
     <H2 style="text-align: center;" class="card-title"> Login With PHP </H2>
